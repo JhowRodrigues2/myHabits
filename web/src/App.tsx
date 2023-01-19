5f4af41ddf758018139b47840eeb8b30e758e2f7
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-01-19 11:20:29 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <div className="">Hello Word!</div>;
+}
+
+export default App;
---
+++ web/src/components/Habits.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Habits = () => {
+  return <div>Habits</div>;
+};
